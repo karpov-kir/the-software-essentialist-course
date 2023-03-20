de1a88d07105110ad31d4380297d07bfd199747a
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-20 20:19:22 +0400

    [docs]: add discrete requirements

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,5 +1,11 @@
+/**
+ * Discrete requirements:
+ * - Tells if a word is a palindrome
+ * - Tells if a phrase is a palindrome
+ * - Case insensitive
+ */
 export class PalindromeChecker {
   isPalindrome(value: string) {
-    return value === value.split('').reverse().join();
+    return value === value.split('').reverse().join('');
   }
 }
