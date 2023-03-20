65964bdd83b0aff9fe3642cbeb5292cd96951afa
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-20 23:09:01 +0400

    [tests]: repharse some test cases

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/PalindromeChecker.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/PalindromeChecker.spec.ts
@@ -3,19 +3,19 @@ import { PalindromeChecker } from './PalindromeChecker';
 const palindromeChecker = new PalindromeChecker();
 
 describe(PalindromeChecker, () => {
-  it('tells that "mom" is a palindrome', () => {
+  it('tells that a word is a palindrome such as "mom"', () => {
     expect(palindromeChecker.isPalindrome('mom')).toBe(true);
   });
 
-  it('tells that "bills" is not a palindrome', () => {
+  it('tells that a word is not a palindrome such as "bills"', () => {
     expect(palindromeChecker.isPalindrome('bills')).toBe(false);
   });
 
-  it('tells that "Mom" is a palindrome', () => {
+  it('tells that a capitalized word is a palindrome such as "Mom"', () => {
     expect(palindromeChecker.isPalindrome('Mom')).toBe(true);
   });
 
-  it('tells that "Was It A Rat I Saw" is a palindrome', () => {
+  it('tells that a phrase is a palindrome such as "Was It A Rat I Saw"', () => {
     expect(palindromeChecker.isPalindrome('Was It A Rat I Saw')).toBe(true);
   });
 
