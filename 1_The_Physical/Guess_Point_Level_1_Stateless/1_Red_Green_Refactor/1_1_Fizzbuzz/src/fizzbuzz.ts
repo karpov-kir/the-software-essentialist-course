a56f6a1be36c5d9c28dd6232dd824068c19e523d
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-20 09:42:50 +0400

    [new]: returns "Fizz" for multiples of 3

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,11 +1,15 @@
 /**
  * Discrete requirements:
  * - outputs numbers as a string
- * - for multiples of three it returns “Fizz”
- * - for multiples of five it returns “Buzz.”
- * - for numbers that are multiples of both three and five, it returns “FizzBuzz.”
+ * - for multiples of three it returns "Fizz"
+ * - for multiples of five it returns "Buzz."
+ * - for numbers that are multiples of both three and five, it returns "FizzBuzz."
  * - takes numbers from 1 to 100
  */
 export const fizzbuzz = (value: number) => {
+    if (value % 3 === 0) {
+        return "Fizz"
+    }
+
     return value.toString();
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("fizzbuzz", () => {
     it('returns a number such as 10 as a string', () => {
         expect(typeof fizzbuzz(10) === 'string').toBeTruthy()
     })
+
+    it('returns "Fizz" for multiples of 3 such as 9', () => {
+        expect(fizzbuzz(9)).toBe("Fizz")
+    })
 });
