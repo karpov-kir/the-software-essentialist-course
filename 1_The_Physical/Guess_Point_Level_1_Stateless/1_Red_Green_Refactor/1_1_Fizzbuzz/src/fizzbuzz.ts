4e8a9f2c9d4410322ef02ec71584bbaee41f5968
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-20 09:39:22 +0400

    [new]: returns numbers as strings

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,11 @@
+/**
+ * Discrete requirements:
+ * - outputs numbers as a string
+ * - for multiples of three it returns “Fizz”
+ * - for multiples of five it returns “Buzz.”
+ * - for numbers that are multiples of both three and five, it returns “FizzBuzz.”
+ * - takes numbers from 1 to 100
+ */
+export const fizzbuzz = (value: number) => {
+    return value.toString();
+}
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import { fizzbuzz } from './fizzbuzz';
 
 describe("fizzbuzz", () => {
-
-
+    it('returns a number such as 10 as a string', () => {
+        expect(typeof fizzbuzz(10) === 'string').toBeTruthy()
+    })
 });
