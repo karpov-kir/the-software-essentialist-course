cd49b2ca37afea8f52a3512d6891f7bc7f62dcd2
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-20 20:10:15 +0400

    [lint]: format code

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -7,29 +7,29 @@
  * - takes numbers from 1 to 100
  */
 export const fizzbuzz = (value: number) => {
-    if (value < 1) {
-        throw new Error('Value is too small')
-    }
+  if (value < 1) {
+    throw new Error('Value is too small');
+  }
 
-    if (value > 100) {
-        throw new Error('Value is too big')
-    }
+  if (value > 100) {
+    throw new Error('Value is too big');
+  }
 
-    if (isMultipleOf(value, 3) && isMultipleOf(value, 5)) {
-        return "FizzBuzz."
-    }
+  if (isMultipleOf(value, 3) && isMultipleOf(value, 5)) {
+    return 'FizzBuzz.';
+  }
 
-    if (isMultipleOf(value, 3)) {
-        return "Fizz"
-    }
+  if (isMultipleOf(value, 3)) {
+    return 'Fizz';
+  }
 
-    if (isMultipleOf(value, 5)) {
-        return "Buzz."
-    }
+  if (isMultipleOf(value, 5)) {
+    return 'Buzz.';
+  }
 
-    return value.toString();
-}
+  return value.toString();
+};
 
 export const isMultipleOf = (value: number, devisor: number) => {
-    return value % devisor === 0
-}
+  return value % devisor === 0;
+};
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,40 +1,40 @@
 import { fizzbuzz } from './fizzbuzz';
 
-describe("fizzbuzz", () => {
-    it('throws an error for numbers less than 1 such as 0', () => {
-        expect(() => fizzbuzz(0)).toThrow()
-    })
+describe('fizzbuzz', () => {
+  it('throws an error for numbers less than 1 such as 0', () => {
+    expect(() => fizzbuzz(0)).toThrow();
+  });
 
-    it('throws an error for numbers greater than 100 such as 101', () => {
-        expect(() => fizzbuzz(101)).toThrow()
-    })
+  it('throws an error for numbers greater than 100 such as 101', () => {
+    expect(() => fizzbuzz(101)).toThrow();
+  });
 
-    it('returns a number such as 10 as a string', () => {
-        expect(typeof fizzbuzz(10) === 'string').toBeTruthy()
-    })
+  it('returns a number such as 10 as a string', () => {
+    expect(typeof fizzbuzz(10) === 'string').toBeTruthy();
+  });
 
-    it('returns "Fizz" for multiples of 3 such as 9', () => {
-        expect(fizzbuzz(9)).toBe("Fizz")
-    })
+  it('returns "Fizz" for multiples of 3 such as 9', () => {
+    expect(fizzbuzz(9)).toBe('Fizz');
+  });
 
-    it('returns "Buzz." for multiples of 5 such as 20', () => {
-        expect(fizzbuzz(20)).toBe("Buzz.")
-    })
+  it('returns "Buzz." for multiples of 5 such as 20', () => {
+    expect(fizzbuzz(20)).toBe('Buzz.');
+  });
 
-    it('returns "FizzBuzz." for multiples of both 3 and 5 such as 15', () => {
-        expect(fizzbuzz(15)).toBe("FizzBuzz.")
-    })
+  it('returns "FizzBuzz." for multiples of both 3 and 5 such as 15', () => {
+    expect(fizzbuzz(15)).toBe('FizzBuzz.');
+  });
 
-    test.each([
-        [3, 'Fizz'],
-        [5, 'Buzz.',],
-        [19, '19'],
-        [30, 'FizzBuzz.'],
-        [33, 'Fizz'],
-        [43, '43'],
-        [60, 'FizzBuzz.'],
-        [100, 'Buzz.',],
-      ])(`given %i returns "%s"`, (value, expected) => {
-        expect(fizzbuzz(value)).toBe(expected);
-      });
+  test.each([
+    [3, 'Fizz'],
+    [5, 'Buzz.'],
+    [19, '19'],
+    [30, 'FizzBuzz.'],
+    [33, 'Fizz'],
+    [43, '43'],
+    [60, 'FizzBuzz.'],
+    [100, 'Buzz.'],
+  ])(`given %i returns "%s"`, (value, expected) => {
+    expect(fizzbuzz(value)).toBe(expected);
+  });
 });
