1e6828a3203454679f72d673814d56f35f358afc
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-20 23:08:53 +0400

    [tests]: add more test cases

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/PalindromeChecker.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/PalindromeChecker.spec.ts
@@ -18,4 +18,20 @@ describe(PalindromeChecker, () => {
   it('tells that "Was It A Rat I Saw" is a palindrome', () => {
     expect(palindromeChecker.isPalindrome('Was It A Rat I Saw')).toBe(true);
   });
+
+  it('tells that an empty string is a palindrome', () => {
+    expect(palindromeChecker.isPalindrome('')).toBe(true);
+  });
+
+  it('tells that a phrase with many spaces is a palindrome such as "Ne   ver Odd or Ev   en"', () => {
+    expect(palindromeChecker.isPalindrome('Ne   ver Odd or Ev   en')).toBe(true);
+  });
+
+  it('tells a word that starts with many spaces is a palindrome such as "  mom"', () => {
+    expect(palindromeChecker.isPalindrome('  mom')).toBe(true);
+  });
+
+  it('tells a word that ends with many spaces is a palindrome such as "mom   "', () => {
+    expect(palindromeChecker.isPalindrome('mom   ')).toBe(true);
+  });
 });
