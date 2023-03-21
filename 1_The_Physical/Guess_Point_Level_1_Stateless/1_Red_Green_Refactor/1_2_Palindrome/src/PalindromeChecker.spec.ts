3f1f24ca4c0fdd78d972cf5f70584fd107cd74f7
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-21 21:14:14 +0400

    [tests]: remove duplication from test case names

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/PalindromeChecker.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/PalindromeChecker.spec.ts
@@ -3,23 +3,23 @@ import { PalindromeChecker } from './PalindromeChecker';
 const palindromeChecker = new PalindromeChecker();
 
 describe(PalindromeChecker, () => {
-  it('tells that a word is a palindrome such as "mom"', () => {
+  it('tells that a word is a palindrome', () => {
     expect(palindromeChecker.isPalindrome('mom')).toBe(true);
   });
 
-  it('tells that a word is not a palindrome such as "bills"', () => {
+  it('tells that a word is not a palindrome', () => {
     expect(palindromeChecker.isPalindrome('bills')).toBe(false);
   });
 
-  it('tells that a capitalized word is a palindrome such as "Mom"', () => {
+  it('tells that a capitalized word is a palindrome', () => {
     expect(palindromeChecker.isPalindrome('Mom')).toBe(true);
   });
 
-  it('tells that a phrase is a palindrome such as "Was It A Rat I Saw"', () => {
+  it('tells that a phrase is a palindrome', () => {
     expect(palindromeChecker.isPalindrome('Was It A Rat I Saw')).toBe(true);
   });
 
-  it('tells that a phrase is not a palindrome such as "I am not a palindrome"', () => {
+  it('tells that a phrase is not a palindrome', () => {
     expect(palindromeChecker.isPalindrome('I am not a palindrome')).toBe(false);
   });
 
@@ -27,15 +27,15 @@ describe(PalindromeChecker, () => {
     expect(palindromeChecker.isPalindrome('')).toBe(true);
   });
 
-  it('tells that a phrase with many spaces is a palindrome such as "Ne   ver Odd or Ev   en"', () => {
+  it('tells that a phrase with many spaces is a palindrome', () => {
     expect(palindromeChecker.isPalindrome('Ne   ver Odd or Ev   en')).toBe(true);
   });
 
-  it('tells a word that starts with many spaces is a palindrome such as "  mom"', () => {
+  it('tells that a word that starts with many spaces is a palindrome', () => {
     expect(palindromeChecker.isPalindrome('  mom')).toBe(true);
   });
 
-  it('tells a word that ends with many spaces is a palindrome such as "mom   "', () => {
+  it('tells that a word that ends with many spaces is a palindrome', () => {
     expect(palindromeChecker.isPalindrome('mom   ')).toBe(true);
   });
 });
