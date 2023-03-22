a88411acaa2e7ac304bb21ebe1985b88a1e3c9a5
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-22 19:38:22 +0400

    [new]: finds a maximum value

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -6,6 +6,12 @@ describe(StatsCalculator, () => {
   it('finds a minimum value', () => {
     const stats = statsCalculator.calculate([1, 2, 3]);
 
-    expect(stats).toEqual({ min: 1 });
+    expect(stats).toEqual(expect.objectContaining({ min: 1 }));
+  });
+
+  it('finds a maximum value', () => {
+    const stats = statsCalculator.calculate([1, 2, 3]);
+
+    expect(stats).toEqual(expect.objectContaining({ max: 3 }));
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -1,15 +1,21 @@
 export class StatsCalculator {
   calculate(sequence: number[]) {
     let min = Infinity;
+    let max = -Infinity;
 
     sequence.forEach((value) => {
       if (value < min) {
         min = value;
       }
+
+      if (value > max) {
+        max = value;
+      }
     });
 
     return {
       min,
+      max,
     };
   }
 }
