61447bc3288e03ad45971aac86a8725b146eacdf
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-23 09:33:18 +0400

    [new]: checks that the start hour is less or equal 24

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -3,7 +3,11 @@ import { TimeValidator } from './index';
 const timeValidator = new TimeValidator();
 
 describe(TimeValidator, () => {
-  it('tells that "13:30 - 15:30" is a valid military time', () => {
+  it('tells that "13:30 - 15:30" is a valid military time range', () => {
     expect(timeValidator.isValidMilitaryTimeRange('13:30 - 15:30')).toBeTruthy();
+  });
+
+  it('tells that "25:30 - 15:30" is not valid military time range', () => {
+    expect(timeValidator.isValidMilitaryTimeRange('25:30 - 15:30')).toBeFalsy();
   });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,5 +1,22 @@
 export class TimeValidator {
-  isValidMilitaryTimeRange(_time: string) {
-    return true;
+  isValidMilitaryTimeRange(timeRange: string) {
+    const { startHours } = getTimeRangeParts(timeRange);
+
+    return parseInt(startHours) <= 24;
   }
 }
+
+const getTimeRangeParts = (timeRange: string) => {
+  const [startTime, endTime] = timeRange.split('-');
+  const [startHours, startMinutes] = startTime.split(':');
+  const [endHours, endMinutes] = endTime.split(':');
+
+  return {
+    startTime,
+    endTime,
+    startHours,
+    endHours,
+    startMinutes,
+    endMinutes,
+  };
+};
