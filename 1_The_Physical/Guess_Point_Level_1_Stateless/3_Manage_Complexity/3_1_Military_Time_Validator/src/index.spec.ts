4ee61c9cb5321661889b5a623aeee11a257c0250
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-23 09:48:26 +0400

    [refactor]: group tests by validity

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -3,15 +3,19 @@ import { TimeValidator } from './index';
 const timeValidator = new TimeValidator();
 
 describe(TimeValidator, () => {
-  it('tells that "13:30 - 15:30" is a valid military time range', () => {
-    expect(timeValidator.isValidMilitaryTimeRange('13:30 - 15:30')).toBeTruthy();
+  describe('valid time', () => {
+    it('tells that "13:30 - 15:30" is a valid military time range', () => {
+      expect(timeValidator.isValidMilitaryTimeRange('13:30 - 15:30')).toBeTruthy();
+    });
   });
 
-  it('tells that "25:30 - 15:30" is not valid military time range', () => {
-    expect(timeValidator.isValidMilitaryTimeRange('25:30 - 15:30')).toBeFalsy();
-  });
+  describe('invalid time because of hours', () => {
+    it('tells that "25:30 - 15:30" is not valid military time range', () => {
+      expect(timeValidator.isValidMilitaryTimeRange('25:30 - 15:30')).toBeFalsy();
+    });
 
-  it('tells that "15:30 - 25:30" is not valid military time range', () => {
-    expect(timeValidator.isValidMilitaryTimeRange('15:30 - 25:30')).toBeFalsy();
+    it('tells that "15:30 - 25:30" is not valid military time range', () => {
+      expect(timeValidator.isValidMilitaryTimeRange('15:30 - 25:30')).toBeFalsy();
+    });
   });
 });
