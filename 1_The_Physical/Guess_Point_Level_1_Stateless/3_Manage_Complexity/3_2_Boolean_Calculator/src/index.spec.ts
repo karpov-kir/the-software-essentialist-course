9b602b5ee4b3935a9ad13b9ea6b56a75ca9dba61
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-24 21:26:59 +0400

    [tests]: add more test cases

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -18,4 +18,24 @@ describe(BooleanCalculator, () => {
   it('tells that "TRUE AND FALSE" is falsy', () => {
     expect(booleanCalculator.isTruthy('TRUE AND FALSE')).toBeFalsy();
   });
+
+  it('tells that "TRUE AND TRUE" is truthy', () => {
+    expect(booleanCalculator.isTruthy('TRUE AND TRUE')).toBeTruthy();
+  });
+
+  it('tells that "FALSE OR TRUE" is truthy', () => {
+    expect(booleanCalculator.isTruthy('FALSE OR TRUE')).toBeTruthy();
+  });
+
+  it('tells that "FALSE OR TRUE AND FALSE" is falsy', () => {
+    expect(booleanCalculator.isTruthy('FALSE OR TRUE AND FALSE')).toBeFalsy();
+  });
+
+  it('rejects "FALSE OR AND FALSE" with an unexpected token error', () => {
+    expect(() => booleanCalculator.isTruthy('FALSE OR AND FALSE')).toThrowError('Expect a boolean token but go "AND"');
+  });
+
+  it('rejects "TRUE AND TRUE FALSE" rejects with an unexpected token error', () => {
+    expect(() => booleanCalculator.isTruthy('TRUE AND TRUE FALSE')).toThrowError('Expect a logic token but go "FALSE"');
+  });
 });
