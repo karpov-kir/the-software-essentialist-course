760b8cf4c934142591d9ae77fe1876011f79e5e4
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-26 17:32:27 +0400

    [new]: add groups support

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -4,51 +4,70 @@ const booleanCalculator = new BooleanCalculator();
 
 describe(BooleanCalculator, () => {
   describe('truthy', () => {
-    it.each(['TRUE', 'TRUE AND TRUE', 'FALSE OR TRUE', 'NOT FALSE', 'TRUE AND NOT FALSE'])(
-      'tells that "%s" is truthy',
-      (booleanExpression) => {
-        expect(booleanCalculator.isTruthy(booleanExpression)).toBeTruthy();
-      },
-    );
+    it.each([
+      'TRUE',
+      'TRUE AND TRUE',
+      'FALSE OR TRUE',
+      'NOT FALSE',
+      'TRUE AND NOT FALSE',
+      '(FALSE OR TRUE) AND TRUE',
+      '(FALSE OR TRUE) AND (FALSE OR TRUE)',
+      '((FALSE OR TRUE) AND (FALSE OR TRUE))',
+      '((FALSE OR (FALSE OR TRUE)) AND (FALSE OR TRUE))',
+      '(TRUE)',
+      '(NOT FALSE)',
+      '((FALSE OR (FALSE OR TRUE)) AND (FALSE OR NOT FALSE))',
+    ])('tells that "%s" is truthy', (booleanExpression) => {
+      expect(booleanCalculator.isTruthy(booleanExpression)).toBeTruthy();
+    });
   });
 
   describe('falsy', () => {
-    it.each(['FALSE', 'TRUE AND FALSE', 'FALSE OR TRUE AND FALSE', 'NOT TRUE', 'TRUE AND NOT TRUE'])(
-      'tells that "%s" is falsy',
-      (booleanExpression) => {
-        expect(booleanCalculator.isTruthy(booleanExpression)).toBeFalsy();
-      },
-    );
+    it.each([
+      'FALSE',
+      'TRUE AND FALSE',
+      'FALSE OR TRUE AND FALSE',
+      'NOT TRUE',
+      'TRUE AND NOT TRUE',
+      '(FALSE OR TRUE) AND FALSE',
+      '((FALSE OR TRUE) AND FALSE',
+      '((FALSE OR (FALSE OR TRUE)) AND (FALSE OR (TRUE AND FALSE)))',
+      '(FALSE)',
+      '(NOT TRUE)',
+      '((FALSE OR (FALSE OR TRUE)) AND (FALSE OR (FALSE AND NOT TRUE)))',
+    ])('tells that "%s" is falsy', (booleanExpression) => {
+      expect(booleanCalculator.isTruthy(booleanExpression)).toBeFalsy();
+    });
   });
 
   describe('unexpected input error', () => {
     it('rejects "TRUE UNEXPECTED FALSE" with an unexpected token error', () => {
       expect(() => booleanCalculator.isTruthy('TRUE UNEXPECTED FALSE')).toThrowError(
-        'Expected a token of type "Logic" but got "UNEXPECTED"',
+        'Expected a token of type "Logic" or "CloseGroup" but got "UNEXPECTED"',
       );
     });
 
     it('rejects "FALSE OR AND FALSE" with an unexpected token error', () => {
       expect(() => booleanCalculator.isTruthy('FALSE OR AND FALSE')).toThrowError(
-        'Expected a token of type "Boolean" or "Not" but got "AND"',
+        'Expected a token of type "Boolean" or "Not" or "OpenGroup" but got "AND"',
       );
     });
 
     it('rejects "FALSE OR NOT NOT FALSE" with an unexpected token error', () => {
       expect(() => booleanCalculator.isTruthy('FALSE OR NOT NOT FALSE')).toThrowError(
-        'Expected a token of type "Boolean" but got "NOT"',
+        'Expected a token of type "Boolean" or "OpenGroup" but got "NOT"',
       );
     });
 
     it('rejects "NOT NOT" with an unexpected token error', () => {
       expect(() => booleanCalculator.isTruthy('NOT NOT')).toThrowError(
-        'Expected a token of type "Boolean" but got "NOT"',
+        'Expected a token of type "Boolean" or "OpenGroup" but got "NOT"',
       );
     });
 
     it('rejects "TRUE AND TRUE FALSE" rejects with an unexpected token error', () => {
       expect(() => booleanCalculator.isTruthy('TRUE AND TRUE FALSE')).toThrowError(
-        'Expected a token of type "Logic" but got "FALSE"',
+        'Expected a token of type "Logic" or "CloseGroup" but got "FALSE"',
       );
     });
   });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,12 +1,23 @@
 const booleanTokens = ['TRUE', 'FALSE'];
 const logicTokens = ['AND', 'OR'];
 const negationToken = 'NOT';
+const openGroupToken = '(';
+const closeGroupToken = ')';
+
+class LinkedListNode<T> {
+  next: LinkedListNode<T> | undefined;
+  previous: LinkedListNode<T> | undefined;
+
+  constructor(public value: T) {}
+}
 
 enum TokenType {
   Boolean = 'Boolean',
   // AND, OR
   Logic = 'Logic',
   Negation = 'Not',
+  OpenGroup = 'OpenGroup',
+  CloseGroup = 'CloseGroup',
   Unknown = 'Unknown',
 }
 
@@ -17,7 +28,15 @@ interface Context {
     // Beginning state
     | undefined;
   logic: 'AND' | 'OR' | undefined;
+  currentGroup: LinkedListNode<Context> | undefined;
 }
+
+const emptyContext: Context = {
+  shouldNegate: false,
+  logic: undefined,
+  state: undefined,
+  currentGroup: undefined,
+};
 
 type Action = {
   tokenType: TokenType;
@@ -32,8 +51,14 @@ type TokenHandler = (
   nextAllowedTokenTypes: TokenType[];
 };
 
+interface TokenIteratorValue {
+  token: string;
+  position: number;
+}
+
 const handlers: Record<string, TokenHandler> = {
   [TokenType.Boolean]: (context, action) => {
+    const nextAllowedTokenTypes = [TokenType.Logic, TokenType.CloseGroup];
     const newContext = { ...context };
     let tokenState = action.token === 'TRUE';
 
@@ -55,58 +80,193 @@ const handlers: Record<string, TokenHandler> = {
 
     return {
       context: newContext,
-      nextAllowedTokenTypes: [TokenType.Logic],
+      nextAllowedTokenTypes,
     };
   },
   [TokenType.Negation]: (context, _action) => {
+    const nextAllowedTokenTypes = [TokenType.Boolean, TokenType.OpenGroup];
     const newContext = { ...context };
 
     newContext.shouldNegate = true;
 
     return {
       context: newContext,
-      nextAllowedTokenTypes: [TokenType.Boolean],
+      nextAllowedTokenTypes,
     };
   },
   [TokenType.Logic]: (context, action) => {
     const newContext = { ...context };
+    const nextAllowedTokenTypes = [TokenType.Boolean, TokenType.Negation, TokenType.OpenGroup];
 
     // TODO get rid of type casting?
     newContext.logic = action.token as 'AND' | 'OR';
 
     return {
       context: newContext,
-      nextAllowedTokenTypes: [TokenType.Boolean, TokenType.Negation],
+      nextAllowedTokenTypes,
+    };
+  },
+  [TokenType.OpenGroup]: (_context, _action) => {
+    const newContext = { ...emptyContext };
+    const nextAllowedTokenTypes = [TokenType.Boolean, TokenType.Negation, TokenType.OpenGroup];
+
+    const currentGroup = newContext.currentGroup;
+
+    const newGroup = new LinkedListNode<Context>(newContext);
+
+    if (currentGroup) {
+      currentGroup.next = newGroup;
+    }
+
+    newGroup.previous = currentGroup;
+    newContext.currentGroup = newGroup;
+
+    return {
+      context: newContext,
+      nextAllowedTokenTypes,
+    };
+  },
+  [TokenType.CloseGroup]: (context, _action) => {
+    const groupState = context.state;
+    const nextAllowedTokenTypes = [TokenType.Logic, TokenType.CloseGroup];
+
+    // A root group (no parent group) e.g. one from "(TRUE AND FALSE) AND (FALSE OR TRUE)"
+    if (!context.currentGroup?.previous) {
+      const newContext = { ...context };
+
+      newContext.currentGroup = undefined;
+      newContext.state = groupState;
+
+      return {
+        context: newContext,
+        nextAllowedTokenTypes,
+      };
+    }
+
+    // Exit from a child context and apply the child group state to the parent context as a simple boolean handler
+    const parentContext = context.currentGroup.previous.value;
+    const { context: newContext } = handlers[TokenType.Boolean](parentContext, {
+      tokenType: TokenType.Boolean,
+      token: groupState ? 'TRUE' : 'FALSE',
+    });
+
+    return {
+      context: newContext,
+      nextAllowedTokenTypes,
     };
   },
 };
 
 export class BooleanCalculator {
   isTruthy(booleanExpression: string) {
-    const tokens = booleanExpression.split(' ');
+    let nextAllowedTokenTypes = [TokenType.Boolean, TokenType.Negation, TokenType.OpenGroup];
+    let context: Context = { ...emptyContext };
 
-    let nextAllowedTokenTypes = [TokenType.Boolean, TokenType.Negation];
-    let context: Context = {
-      shouldNegate: false,
-      logic: undefined,
-      state: undefined,
-    };
+    const tokenIterator = getTokenIterator(booleanExpression);
 
-    tokens.forEach((token) => {
-      const tokenType = getTokenType(token);
+    while (true) {
+      const {
+        value: { token, position },
+        done,
+      } = tokenIterator.next();
 
-      if (!nextAllowedTokenTypes.includes(tokenType)) {
-        throw new Error(`Expected a token of type "${nextAllowedTokenTypes.join('" or "')}" but got "${token}"`);
+      try {
+        ({ nextAllowedTokenTypes, context } = handleToken(token, context, nextAllowedTokenTypes));
+      } catch (unknownError) {
+        const error = unknownError as Error;
+        throw new Error(`Error at position ${position}: ${error.message}`);
       }
 
-      const handler = handlers[tokenType];
-      const action = { tokenType, token };
-
-      ({ nextAllowedTokenTypes, context } = handler(context, action));
-    });
+      if (done) {
+        break;
+      }
+    }
 
     return Boolean(context.state);
   }
+}
+
+function getTokenIterator(booleanExpression: string): Iterator<TokenIteratorValue> {
+  if (!booleanExpression) {
+    throw new Error('Boolean expression must not be empty');
+  }
+
+  let position = 0;
+
+  const geSpaceCountStartingFrom = (startPosition: number) => {
+    let count = 0;
+
+    for (let l = booleanExpression.length; startPosition < l; startPosition++) {
+      const character = booleanExpression[startPosition];
+
+      if (/\s/.test(character)) {
+        count++;
+      } else {
+        break;
+      }
+    }
+
+    return count;
+  };
+
+  return {
+    next() {
+      let currentToken = '';
+
+      for (let l = booleanExpression.length; position < l; position++) {
+        const character = booleanExpression[position];
+        const isSpace = /\s/.test(character);
+        const isEnd = position === l - 1;
+        const isCurrentGroupToken = isGroupToken(character);
+        const isNextGroupToken = isGroupToken(booleanExpression[position + 1]);
+        const isTokenComplete = isSpace || isEnd || isCurrentGroupToken || isNextGroupToken;
+
+        if (!isSpace) {
+          currentToken += character;
+        }
+
+        if (isTokenComplete) {
+          if (isCurrentGroupToken || isNextGroupToken) {
+            position++;
+          }
+
+          position += geSpaceCountStartingFrom(position);
+          const newIsEnd = position === l - 1;
+
+          return {
+            value: {
+              token: currentToken,
+              position,
+            },
+            done: newIsEnd,
+          };
+        }
+      }
+
+      throw new Error('Iterator has already been finished');
+    },
+  };
+}
+
+function handleToken(
+  token: string,
+  context: Context,
+  nextAllowedTokenTypes: TokenType[],
+): { context: Context; nextAllowedTokenTypes: TokenType[] } {
+  const tokenType = getTokenType(token);
+
+  if (!nextAllowedTokenTypes.includes(tokenType)) {
+    throw new Error(`Expected a token of type "${nextAllowedTokenTypes.join('" or "')}" but got "${token}"`);
+  }
+
+  const handler: TokenHandler = handlers[tokenType];
+  const action: Action = { tokenType, token };
+
+  return handler(context, action);
+}
+
+function isGroupToken(token: string): boolean {
+  return token === openGroupToken || token === closeGroupToken;
 }
 
 function getTokenType(token: string): TokenType {
@@ -120,6 +280,14 @@ function getTokenType(token: string): TokenType {
 
   if (token === negationToken) {
     return TokenType.Negation;
+  }
+
+  if (token === openGroupToken) {
+    return TokenType.OpenGroup;
+  }
+
+  if (token === closeGroupToken) {
+    return TokenType.CloseGroup;
   }
 
   return TokenType.Unknown;
