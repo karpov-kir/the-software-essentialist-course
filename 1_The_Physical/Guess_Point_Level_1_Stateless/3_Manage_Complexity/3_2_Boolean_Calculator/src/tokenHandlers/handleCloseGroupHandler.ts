b61150d3e0801cd1d3c2c166e7acde642a61e656
Kirill Karpov <karpov.kir.v@gmail.com>
2023-03-27 19:51:41 +0400

    [refactor]: make close group handler more granular

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/tokenHandlers/handleCloseGroupHandler.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/tokenHandlers/handleCloseGroupHandler.ts
@@ -1,19 +1,29 @@
 import { handleBooleanToken } from './handleBooleanToken';
 import { Token } from '../tokens';
-import { TokenHandler } from './tokenHandlers';
+import { TokenHandler, Action, Context } from './tokenHandlers';
 
 export const handleCloseGroupToken: TokenHandler = (context, _action) => {
   const nextAllowedTokens = [Token.And, Token.Or, Token.CloseGroup];
-  const groupState = context.state;
+  const finalGroupState = Boolean(context.state);
 
   if (!context.parentContext) {
     throw new Error('Cannot close a group without a parent context');
   }
 
-  // Exit from a child context and apply the child group state to the parent context as a simple boolean handler
-  const [newContext] = handleBooleanToken(context.parentContext, {
-    token: groupState ? Token.True : Token.False,
-  });
+  return applyGroupFinalStateToParentContext(context.parentContext, finalGroupState, nextAllowedTokens);
+};
+
+// Exit from a child context and apply the child group final state to the parent context as a simple boolean handler
+const applyGroupFinalStateToParentContext = (
+  parentContext: Context,
+  finalGroupState: boolean,
+  nextAllowedTokens: Token[],
+): ReturnType<TokenHandler> => {
+  const action: Action = {
+    token: finalGroupState ? Token.True : Token.False,
+  };
+
+  const [newContext] = handleBooleanToken(parentContext, action);
 
   return [newContext, nextAllowedTokens];
 };
