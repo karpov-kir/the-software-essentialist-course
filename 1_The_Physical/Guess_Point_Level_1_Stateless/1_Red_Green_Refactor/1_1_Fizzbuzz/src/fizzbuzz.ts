b4c6f3a57a6006b3f5cd9c56549261c9a2d0913d
Kirill Karpov <85180621+karpov-kir@users.noreply.github.com>
2023-04-12 14:14:15 +0400

    feat: FizzBuzz 1st attempt (#1)

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,35 @@
+/**
+ * Discrete requirements:
+ * - outputs numbers as a string
+ * - for multiples of three it returns "Fizz"
+ * - for multiples of five it returns "Buzz."
+ * - for numbers that are multiples of both three and five, it returns "FizzBuzz."
+ * - takes numbers from 1 to 100
+ */
+export const fizzbuzz = (value: number) => {
+  if (value < 1) {
+    throw new Error('Value is too small');
+  }
+
+  if (value > 100) {
+    throw new Error('Value is too big');
+  }
+
+  if (isMultipleOf(value, 3) && isMultipleOf(value, 5)) {
+    return 'FizzBuzz.';
+  }
+
+  if (isMultipleOf(value, 3)) {
+    return 'Fizz';
+  }
+
+  if (isMultipleOf(value, 5)) {
+    return 'Buzz.';
+  }
+
+  return value.toString();
+};
+
+export const isMultipleOf = (value: number, devisor: number) => {
+  return value % devisor === 0;
+};
