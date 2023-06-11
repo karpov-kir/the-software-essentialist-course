eb3dcfc6f6ca55c89307eba870814238ef24fc06
Kirill Karpov <karpov.kir.v@gmail.com>
2023-06-11 10:25:14 +0400

    [refactor]: reduce cyclomatic complexity of handleBooleanToken

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/tokenHandlers/handleBooleanToken.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/tokenHandlers/handleBooleanToken.ts
@@ -1,26 +1,37 @@
 import { Token } from './../tokens';
-import { TokenHandler } from './tokenHandlers';
+import { Action, Context, TokenHandler } from './tokenHandlers';
 
 export const handleBooleanToken: TokenHandler = (context, action) => {
   const nextAllowedTokens = [Token.And, Token.Or, Token.CloseGroup];
+  const tokenState = computeTokenState(context, action);
+  const nextState = computeNextState(tokenState, context.logic, context.state);
   const newContext = { ...context };
-  let tokenState = action.token === Token.True;
-
-  if (newContext.shouldNegate) {
-    tokenState = !tokenState;
-    newContext.shouldNegate = false;
-  }
-
-  if (newContext.state === undefined) {
-    newContext.state = tokenState;
-  } else if (context.logic === Token.And) {
-    newContext.state = newContext.state && tokenState;
-  } else {
-    newContext.state = newContext.state || tokenState;
-  }
 
   newContext.shouldNegate = false;
   newContext.logic = undefined;
+  newContext.state = nextState;
 
   return [newContext, nextAllowedTokens];
 };
+
+function computeTokenState(context: Context, action: Action) {
+  const tokenState = action.token === Token.True;
+
+  if (context.shouldNegate) {
+    return !tokenState;
+  }
+
+  return tokenState;
+}
+
+function computeNextState(tokenState: boolean, logic?: Token.And | Token.Or, state?: boolean) {
+  if (state === undefined) {
+    return tokenState;
+  }
+
+  if (logic === Token.And) {
+    return state && tokenState;
+  }
+
+  return state || tokenState;
+}
