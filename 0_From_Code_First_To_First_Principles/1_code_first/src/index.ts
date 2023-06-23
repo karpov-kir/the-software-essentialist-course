9b1f068322249062d4afce0fb04650af57550c6e
Kirill Karpov <karpov.kir.v@gmail.com>
2023-06-23 10:24:31 +0400

    Implement user endpoints using GitHub Copilot X

---
+++ 0_From_Code_First_To_First_Principles/1_code_first/src/index.ts
@@ -0,0 +1,101 @@
+import express, { Request, Response } from 'express';
+import { PrismaClient, User, Prisma } from '@prisma/client';
+
+class UserService {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async createUser(user: Prisma.UserCreateInput): Promise<User> {
+    return await this.prisma.user.create({
+      data: user,
+    });
+  }
+
+  async updateUser(email: string, user: Prisma.UserUpdateInput): Promise<User> {
+    return await this.prisma.user.update({
+      where: { email },
+      data: user,
+    });
+  }
+
+  async getUserByEmail(email: string): Promise<User | null> {
+    return await this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+}
+
+const userService = new UserService();
+const app = express();
+
+app.use(express.json());
+
+app.post('/users', async (req: Request, res: Response) => {
+  try {
+    const { firstName, lastName, email } = req.body;
+
+    // Validate request body
+    if (!firstName || !lastName || !email) {
+      return res.status(400).json({ message: 'First name, last name, and email are required' });
+    }
+
+    const user = await userService.createUser({
+      firstName,
+      lastName,
+      email,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.put('/users/:email', async (req: Request, res: Response) => {
+  try {
+    const { firstName, lastName } = req.body;
+    const { email } = req.params;
+
+    // Validate request body
+    if (!firstName && !lastName) {
+      return res.status(400).json({ message: 'First name or last name is required' });
+    }
+
+    const user = await userService.updateUser(email, {
+      firstName,
+      lastName,
+    });
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.get('/users/:email', async (req: Request, res: Response) => {
+  try {
+    const { email } = req.params;
+
+    const user = await userService.getUserByEmail(email);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
