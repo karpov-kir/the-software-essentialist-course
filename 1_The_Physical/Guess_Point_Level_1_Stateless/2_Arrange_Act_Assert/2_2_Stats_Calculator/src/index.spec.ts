d8fd9b27a378484cdd3a7cc41e3cf34be8c09b60
Kirill Karpov <85180621+karpov-kir@users.noreply.github.com>
2024-10-28 21:54:38 +0400

    feat: stats calculator 1st attempt (#5)

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,59 @@
+import { NonEmptySequence, StatsCalculator } from './';
 
-describe('stats calculator', () => {
+const statsCalculator = new StatsCalculator();
 
+type TestSample = Array<{
+  values: NonEmptySequence;
+  result: number;
+}>;
 
-})
+describe(StatsCalculator, () => {
+  describe('minimum value', () => {
+    it.each([
+      { values: [1, 2, 3], result: 1 },
+      { values: [2, 4, 21, -8, 53, 40], result: -8 },
+    ] as TestSample)(`knows that $result is the minimum value in $values`, ({ values, result }) => {
+      const stats = statsCalculator.calculate(values);
+
+      expect(stats).toEqual(expect.objectContaining({ min: result }));
+    });
+  });
+
+  describe('maximum value', () => {
+    it.each([
+      { values: [1, 2, 3], result: 3 },
+      { values: [2, 4, 21, -8, 53, 40], result: 53 },
+    ] as TestSample)(`knows that $result is the maximum value in $values`, ({ values, result }) => {
+      const stats = statsCalculator.calculate(values);
+
+      expect(stats).toEqual(expect.objectContaining({ max: result }));
+    });
+  });
+
+  describe('number of elements', () => {
+    it.each([
+      { values: [1, 2, 3], result: 3 },
+      { values: [2, 4, 21, -8, 53, 40], result: 6 },
+    ] as TestSample)(`knows that $result is the number of elements in $values`, ({ values, result }) => {
+      const stats = statsCalculator.calculate(values);
+
+      expect(stats).toEqual(expect.objectContaining({ numberOfElements: result }));
+    });
+  });
+
+  describe('average value', () => {
+    it.each([
+      { values: [1, 2, 3], result: 2 },
+      { values: [2, 4, 21, -8, 53, 40], result: 18.666666666666668 },
+    ] as TestSample)(`knows that $result is the average value of $values`, ({ values, result }) => {
+      const stats = statsCalculator.calculate(values);
+
+      expect(stats).toEqual(expect.objectContaining({ average: result }));
+    });
+  });
+
+  it('does not accept an empty sequence', () => {
+    // @ts-expect-error cannot accept empty sequence
+    statsCalculator.calculate([]);
+  });
+});
